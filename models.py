20d1eec1a4e917865c77f5b628f3630c0348e5fe
Yoav Navon <yanavon@uc.cl>
2019-10-11 22:27:01 -0300

    Add CNN activation map vis

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten, Input
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 
@@ -21,6 +21,18 @@ def get_model_1(input_shape, num_classes):
     model.add(Activation('softmax'))
 
     return model
+
+def get_func_model_1(input_shape, num_classes):
+    input_layer = Input(input_shape)
+    conv1 = Conv2D(32, (3, 3), padding='same', activation='relu')(input_layer)
+    conv2 = Conv2D(32, (3, 3), padding='same', activation='relu')(conv1)
+    max_pool = MaxPooling2D(pool_size=(2, 2))(conv2)
+    flat = Flatten()(max_pool)
+    dense1 = Dense(10, activation='relu')(flat)
+    output = Dense(num_classes, activation='softmax')(dense1)
+    model = Model(inputs=input_layer, outputs=output)
+    return model
+
 
 def get_model_2(input_shape, num_classes):
     model = Sequential()
